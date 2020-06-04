7b0798e43e701035279c97b2787c7484d37175d5
Vicente Manzano <46681871+DevVincent@users.noreply.github.com>
2020-06-05 00:56:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
-//create db connection
- //const db = mysql.createConnection({
-  //  host: 'localhost',
-  //  user: 'vincyhbp_root',
-  //  password: 'Basededatos008',
-  //  database: 'vincyhbp_nodesql'
-//});
+
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
