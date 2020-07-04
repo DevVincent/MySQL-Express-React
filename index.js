f5dbb5aacb14078b0a12c37d2f73340b6be7d42b
Vicente Manzano <46681871+DevVincent@users.noreply.github.com>
2020-07-04 21:51:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql')
-const urlencodedParser = bodyParser.urlencoded({extended:false});
+const urlencodedParser = bodyParser.urlencoded({extended:false})
 
 const app = express();
 const PORT = process.env.PORT || 4000;
